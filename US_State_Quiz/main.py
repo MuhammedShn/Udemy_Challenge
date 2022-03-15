a7ea3eb783d70df1a551e4b66280e07271b9d2a3
Muhammed Şahin <muhammedsahin9098@gmail.com>
2022-03-15 22:14:14 +0300

    First Commit

---
+++ US_State_Quiz/main.py
@@ -0,0 +1,45 @@
+import turtle
+import pandas
+
+FONT = ("Arial",8, "normal")
+SCREEN_SIZE = [725, 491]
+
+data = pandas.read_csv("50_states.csv")
+guessed_state = []
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+screen.setup(width=SCREEN_SIZE[0], height=SCREEN_SIZE[1])
+screen.addshape(image)
+turtle.shape(image)
+
+while len(guessed_state) < 51:
+
+    answer = screen.textinput(title=f"{len(guessed_state)}/50 States Correct", prompt="What's another state's name?")
+    answer = answer.title()
+
+    if answer == "Exit":
+        state_to_learn = data["state"].to_list()
+        for guessed_state_name in guessed_state:
+            state_to_learn.remove(guessed_state_name)
+
+        state_to_learn = {"state": state_to_learn}
+        data_frame = pandas.DataFrame(state_to_learn)
+        data_frame.to_csv("state_to_learn.csv")
+        break
+    if answer in data["state"].values:
+        if answer not in guessed_state:
+            state = turtle.Turtle()
+            state.hideturtle()
+            state.penup()
+            state_data = data[data.state == answer]
+            guessed_state.append(answer)
+            state.goto(int(state_data.x),int(state_data.y))
+            state.write(arg=state_data.state.item(), move=False, align="center", font=FONT)
+        else:
+            pass
+    else:
+        pass
+
+
